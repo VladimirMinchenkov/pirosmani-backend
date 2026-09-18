a28d9f1717d3545b7eeddf83ed82772409838070
Uladzimir Minchankou <vladimir@MacBook-Pro-Uladzimir.local>
2026-09-18 22:57:13 +0300

    Added uniqueness validation on name in Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@
 class Category < ApplicationRecord
   has_many :menu_items, dependent: :nullify
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :position, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Category, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+
+    it 'validates uniqueness of name' do
+      create(:category, name: 'Unique')
+      expect(build(:category, name: 'Unique')).not_to be_valid
+    end
     it { should validate_numericality_of(:position).only_integer.is_greater_than_or_equal_to(0) }
   end
 end
